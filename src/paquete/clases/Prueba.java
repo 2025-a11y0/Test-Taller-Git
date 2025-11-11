eba45f06cc7f61bb12aca5828a353229eb8732b1
L33222 <L33222@lab.aca>
2025-11-11 12:23:48 -0500

    Primer commit

---
+++ src/paquete/clases/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete.clases;
+
+/**
+ *
+ * @author L33222
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+
+    
+}
