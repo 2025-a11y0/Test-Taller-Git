6ad6bed08cf9c4ae3548066b134c7c5b01dedf51
L33222 <L33222@lab.aca>
2025-11-11 12:33:11 -0500

    Agregue los getters y setters en la clase Login

--- src/paquete/clases/Login.java
+++ src/paquete/clases/Login.java
@@ -18,6 +18,22 @@ public class Login {
         this.user = user;
         this.pass = pass;
     }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
     
     
 }
